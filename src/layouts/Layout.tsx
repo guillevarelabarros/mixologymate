7f3bb9b2ae86e3a8745df03af55c7a4cc9b71fe9
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-14 13:01:15 -0600

    06

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+
+export default function Layout() {
+  return (
+    <>
+        <Header />
+
+        <main className='container mx-auto py-16'>
+            <Outlet />
+        </main>
+    </>
+  )
+}
