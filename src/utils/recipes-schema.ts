fb1b373bcfd913685aeb50e6d79198efbba814a8
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-16 12:14:52 -0600

    15

---
+++ src/utils/recipes-schema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+export const CategoriesAPIResponseSchema = z.object({
+    drinks: z.array(
+        z.object({
+            strCategory: z.string()
+        })
+    )
+})
