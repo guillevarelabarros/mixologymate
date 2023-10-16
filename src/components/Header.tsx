e2706667a6ab8261f4c19f8ca0199d5a64caee2a
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-16 13:18:07 -0600

    19

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useMemo, useState } from 'react'
+import { ChangeEvent, FormEvent, useEffect, useMemo, useState } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 import { useAppStore } from '../stores/useAppStore'
 
@@ -13,6 +13,7 @@ export default function Header() {
 
     const fetchCategories = useAppStore((state) => state.fetchCategories)
     const categories = useAppStore((state) => state.categories)
+    const searchRecipes = useAppStore((state) => state.searchRecipes)
 
     useEffect(() => {
         fetchCategories()
@@ -23,6 +24,18 @@ export default function Header() {
             ...searchFilters,
             [e.target.name] : e.target.value
         })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        // TODO: Validar
+        if(Object.values(searchFilters).includes('')) {
+            console.log('Todos los campos son obligatorios')
+            return
+        }
+        // Consultar las recetas
+        searchRecipes(searchFilters)
     }
 
     return (
@@ -50,6 +63,7 @@ export default function Header() {
                 { isHome && (
                     <form
                         className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'
+                        onSubmit={handleSubmit}
                     >
                         <div className='space-y-4'>
                             <label 
@@ -78,7 +92,7 @@ export default function Header() {
                                 name='category'
                                 className='p-3 w-full rounded-lg focus:outline-none'
                                 onChange={handleChange}
-                                value={searchFilters.ingredient}
+                                value={searchFilters.category}
                             >
                                 <option value="">-- Seleccione --</option>
                                 {categories.drinks.map( category => (
