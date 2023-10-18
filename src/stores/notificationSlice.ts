904cec96504c4796f7603ec7d1abf7899c692a54
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-18 11:32:51 -0600

    40

--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -10,9 +10,10 @@ type Notification = {
 export type NotificationSliceType = {
     notification: Notification
     showNotification: (payload: Pick<Notification, 'text' | 'error'>) => void
+    hideNotification: () => void
 }
 
-export const createNotificationSlice : StateCreator<NotificationSliceType & FavoritesSliceType, [], [], NotificationSliceType> = (set) => ({
+export const createNotificationSlice : StateCreator<NotificationSliceType & FavoritesSliceType, [], [], NotificationSliceType> = (set, get) => ({
     notification: {
         text: '',
         error: false,
@@ -25,6 +26,18 @@ export const createNotificationSlice : StateCreator<NotificationSliceType & Favo
                 error: payload.error,
                 show: true
             }
+        })
+        setTimeout(() => {
+            get().hideNotification()
+        }, 5000);
+    },
+    hideNotification: () => {
+        set({
+            notification: {
+                text: '',
+                error: false,
+                show: false
+            },
         })
     }
 })
